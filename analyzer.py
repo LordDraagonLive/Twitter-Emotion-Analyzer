1ca37150a7450e5db480590ea20202d9650581ab
Buddhi Adhikari <buddhiadikari@live.com>
2018-06-19 15:18:11 +0100

    displaying tweets

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,11 @@
+'''
+This program will analyze the sentiment level of the person who tweets about 
+a specific topic and will give out a user friendly score to the user about
+the tweets sentiment level. 
+
+This Program intends to showcase a glimpse of common natural language processing (NLP)
+
+'''
 import tweepy
 from textblob import TextBlob
 import config
@@ -8,4 +16,27 @@ consumer_secret = config.consumer_secret
 
 access_token = config.access_token
 access_token_secret = config.access_token_secret
+
+# Authenticating the app with twitter via tweepy's OAuthHandler()
+# This method takes in consumer_key and consumer_secret as params respectively
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+
+# Will set the access token and access token secret to our OAuthHandler var
+auth.set_access_token(access_token,access_token_secret)
+
+# Will create a var off tweepy API object
+# this can be used for all twitter methods that we need for our app
+api = tweepy.API(auth)
+
+# Search for all the tweets with the specified keyword
+public_tweets = api.search('Trump')
+
+for tweet in public_tweets:
+    print(tweet.text)
+    # Performing sentiment analysis
+    analysis = TextBlob(tweet.text)
+    # Printing the analysis result
+    print(analysis.sentiment)
+
+
 
