a57c8338747da27fcec44d81331da0ff16ac5d3a
Buddhi Adhikari <buddhiadikari@live.com>
2018-06-19 14:50:38 +0100

    first commit

---
+++ analyzer.py
@@ -0,0 +1,11 @@
+import tweepy
+from textblob import TextBlob
+import config
+
+# Twitter consumer keys and access tokens
+consumer_key = config.consumer_key
+consumer_secret = config.consumer_secret
+
+access_token = config.access_token
+access_token_secret = config.access_token_secret
+
